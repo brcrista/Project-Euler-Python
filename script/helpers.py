9bb4244fd922cb489c8ecfa0189090e28f022ccf
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-20 12:49:02 -0500

    Fix doctest in helpers.py

--- script/helpers.py
+++ script/helpers.py
@@ -14,8 +14,9 @@ def import_file(module_name, file_path):
     Recipe from https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly.
 
     Example:
-    >>> problem = import_file('problem_01', '../src/page_01/problem_01.py')
+    >>> problem = import_file('problem_01', 'src/page_01/problem_01.py')
     >>> problem.solution()
+    233168
     """
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
