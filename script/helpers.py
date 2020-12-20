f0f0bbd80c603caa57a438faaab9f6f14c8c06cf
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-19 20:16:33 -0500

    Add script/solution

---
+++ script/helpers.py
@@ -0,0 +1,30 @@
+import importlib.util
+import sys
+
+def print_error(msg):
+    """
+    Print a message to the stderr stream.
+    """
+    print(msg, file=sys.stderr)
+
+def try_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+import sys
+
+def import_file(module_name, file_path):
+    """
+    Recipe from https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly.
+
+    Example:
+    >>> solution = import_file('solution', '../euler/0001/solution.py')
+    >>> solution.solution()
+    """
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
--- script/project.py
+++ script/project.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from helpers import print_error, try_int
+
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
 # The repository root
@@ -20,18 +22,14 @@ class InDirectory:
     def __exit__(self, exc_type, exc_value, traceback):
         os.chdir(self.origin)
 
-def print_error(msg):
-    """
-    Print a message to the stderr stream.
-    """
-    print(msg, file=sys.stderr)
-
 def index_solutions(project_root):
     """
     Map a problem number to the filepath for each solution relative to the project root.
     """
     euler = os.path.join(project_root, "euler")
     os.chdir(euler)
+
     # Don't assume that solutions to all problems are here.
     # Some might have been skipped.
-    return { int(sln): os.path.join(euler, sln) for sln in os.listdir() }
+    # Just ignore files that don't fit the pattern (such as .DS_STORE).
+    return { try_int(sln): os.path.join(euler, sln) for sln in os.listdir() if try_int(sln) is not None }
