30ac990eece642b7da87925bb94979c20760ac6b
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-20 15:39:26 -0500

    simplify slicing

--- src/page_01/problem_13.py
+++ src/page_01/problem_13.py
@@ -5,7 +5,7 @@ from core.fileio import stream_file
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
 def first_10_digits(n: int) -> str:
-    return str(n)[0:10]
+    return str(n)[:10]
 
 def solution():
     lines = stream_file(os.path.join(scriptdir, 'problem_13_data.txt'))
