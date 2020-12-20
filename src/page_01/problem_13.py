c226142a3df0305ea56de8a1d995ecac492e7acb
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-20 15:38:46 -0500

    Add fileio and give it a spin

--- src/page_01/problem_13.py
+++ src/page_01/problem_13.py
@@ -1,17 +1,12 @@
 import os
 
-from typing import Iterator
+from core.fileio import stream_file
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
-
-def read_numbers_from_file(filename: str) -> Iterator[int]:
-    """Read the list of 50-digit numbers from `filename`"""
-    with open(os.path.join(scriptdir, filename)) as fd:
-        for line in fd.readlines():
-            yield int(line)
 
 def first_10_digits(n: int) -> str:
     return str(n)[0:10]
 
 def solution():
-    return first_10_digits(sum(read_numbers_from_file('problem_13_data.txt')))
+    lines = stream_file(os.path.join(scriptdir, 'problem_13_data.txt'))
+    return first_10_digits(sum(int(line) for line in lines))
