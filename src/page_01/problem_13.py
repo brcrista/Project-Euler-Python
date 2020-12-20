cbacea8732b8ff7ffcd747d6fbbae0410a721922
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-20 15:44:40 -0500

    use read_file in 22

--- src/page_01/problem_13.py
+++ src/page_01/problem_13.py
@@ -1,6 +1,6 @@
 import os
 
-from core.fileio import stream_file
+from core import fileio
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,5 +8,6 @@ def first_10_digits(n: int) -> str:
     return str(n)[:10]
 
 def solution():
-    lines = stream_file(os.path.join(scriptdir, 'problem_13_data.txt'))
+    data_file = os.path.join(scriptdir, 'problem_13_data.txt')
+    lines = fileio.stream_file(data_file)
     return first_10_digits(sum(int(line) for line in lines))
