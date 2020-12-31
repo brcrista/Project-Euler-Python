358904936421a550e7cbc798bc53b22918f30dc4
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-30 21:52:29 -0500

    Problems 1 and 2

--- src/page_01/problem_02.py
+++ src/page_01/problem_02.py
@@ -3,9 +3,9 @@ from itertools import takewhile
 from mathtools.number_theory import even
 from mathtools.combinatorics import fibonacci_numbers
 
-def even_fibonacci_numbers(n: int) -> int:
+def sum_even_fibonacci_numbers(n: int) -> int:
     """The sum of even Fibonacci numbers less than `n`."""
-    return sum(fib for fib in takewhile(lambda x: x < n, fibonacci_numbers()) if even(fib))
+    return sum(fib for fib in takewhile(lambda x: x <= n, fibonacci_numbers()) if even(fib))
 
 def solution():
-    return even_fibonacci_numbers(4000000)
+    return sum_even_fibonacci_numbers(4000000)
