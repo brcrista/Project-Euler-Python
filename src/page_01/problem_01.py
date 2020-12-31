c315b83d96c99ac08f93ee7db3411feccfd8a98a
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-12-31 16:24:54 -0500

    Rename functions in problems 1 and 2 (#15)
    I had named these following the actual names of the problems on Project Euler, but I'm going to rename things to more accurately reflect what they do.
    
    I'm just taking problems 1 and 2 for now because I'm going to make some other changes next that touch these files.

--- src/page_01/problem_01.py
+++ src/page_01/problem_01.py
@@ -1,7 +1,7 @@
 from mathtools.number_theory import divides
 
-def multiples_of_3_and_5(n: int) -> int:
+def sum_multiples_of_3_and_5(n: int) -> int:
     return sum(x for x in range(0, n) if divides(x, 3) or divides(x, 5))
 
 def solution():
-    return multiples_of_3_and_5(1000)
+    return sum_multiples_of_3_and_5(1000)
