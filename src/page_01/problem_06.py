b79dea3a098f3ba907afe2f5560e2d277e4fe76e
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-02-13 20:27:50 -0500

    Problem 6 renames

--- src/page_01/problem_06.py
+++ src/page_01/problem_06.py
@@ -12,14 +12,14 @@ Hence, the difference between the sum of the squares of the first ten natural nu
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
-def square_of_sum(n: int) -> int:
-    return sum(range(1, n + 1)) ** 2
-
 def sum_of_squares(n: int) -> int:
     return sum(x ** 2 for x in range(1, n + 1))
 
+def square_of_sums(n: int) -> int:
+    return sum(range(1, n + 1)) ** 2
+
 def sum_square_difference(n: int) -> int:
-    return square_of_sum(n) - sum_of_squares(n)
+    return square_of_sums(n) - sum_of_squares(n)
 
 def solution():
     return sum_square_difference(100)
