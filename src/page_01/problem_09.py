4bdecf357972c8bd92d58527b201a1bba73bf535
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-02 09:35:12 -0500

    Problem 9 redux (#20)
    * Decompose into more steps
    
    * Fix doc comment
    
    * Add test for is_pythagorean

--- src/page_01/problem_09.py
+++ src/page_01/problem_09.py
@@ -1,24 +1,32 @@
 """
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which a^2 + b^2 = c^2
 
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product a * b * c.
 """
 
 from itertools import count
+from typing import Iterator, Tuple
 
-def pythagorean_triple(a: int, b: int, c: int) -> int:
-    assert a < b < c
+from mathtools import product
+
+def is_pythagorean(a: int, b: int, c: int) -> bool:
+    # Using * is about 7x faster than using **
     return (a * a) + (b * b) == (c * c)
 
-def special_pythagorean_triple() -> int:
-    """The product of the Pythagorean triple whose sum equals 1000."""
-    return next(a * b * c
-        for c in count(3)
-        for b in range(2, c)
-        for a in range(1, b)
-        if pythagorean_triple(a, b, c) and a + b + c == 1000)
+pythagorean_triples = (
+    (a, b, c)
+    for c in count(3)
+    for b in range(2, c)
+    for a in range(1, b)
+    if is_pythagorean(a, b, c)
+)
 
-solution = special_pythagorean_triple
+def pythagorean_sum_equals(n: int) -> Iterator[Tuple[int, int, int]]:
+    """The Pythagorean triples whose sum equals `n`.."""
+    return filter(lambda xs: sum(xs) == 1000, pythagorean_triples)
+
+def solution() -> int:
+    return product(next(pythagorean_sum_equals(1000)))
