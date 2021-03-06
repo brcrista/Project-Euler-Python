237706fba70e7facda94db9ddc8025cb1b519bb8
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-06 09:52:20 -0500

    Add test for triangle numbers

--- src/page_01/problem_12_test.py
+++ src/page_01/problem_12_test.py
@@ -1,4 +1,9 @@
-from .problem_12 import first_triangle_number_with_more_divisors_than, solution
+from core.iterable import take
+
+from .problem_12 import first_triangle_number_with_more_divisors_than, solution, triangle_numbers
+
+def test_triangle_numbers():
+    assert list(take(10, triangle_numbers())) == [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
 
 def test_first_triangle_number_with_more_divisors_than():
     assert first_triangle_number_with_more_divisors_than(5) == 28
