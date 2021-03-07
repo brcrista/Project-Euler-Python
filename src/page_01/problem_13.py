233caafaa3baa1c3c537ceac46312e303ae4f72f
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-06 19:50:14 -0500

    Split out function in Problem 13

--- src/page_01/problem_13.py
+++ src/page_01/problem_13.py
@@ -5,13 +5,17 @@ Work out the first ten digits of the sum of the one hundred 50-digit numbers in 
 import os
 
 from core import fileio
+from typing import List
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 
 def first_10_digits(n: int) -> str:
     return str(n)[:10]
 
+def first_10_digits_of_sum(lines: List[str]) -> str:
+    return first_10_digits(sum(int(line) for line in lines))
+
 def solution():
     data_file = os.path.join(scriptdir, 'problem_13_data.txt')
     lines = fileio.stream_file(data_file)
-    return first_10_digits(sum(int(line) for line in lines))
+    return first_10_digits_of_sum(lines)
