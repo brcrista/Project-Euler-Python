3bdc65a17a0b67d99894695aeab595d8ab39694f
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-03-27 13:03:40 -0400

    Problem 19 redux (#22)
    * Use day values corresponding to the `calendar` module
    
    * doc comment for day_mod
    
    * Add days_in_year
    
    * Separate logic for generating days and counting days

--- src/page_01/problem_19.py
+++ src/page_01/problem_19.py
@@ -14,17 +14,22 @@ How many Sundays fell on the first of the month during the twentieth century (1 
 """
 
 from enum import IntEnum
+from typing import Iterator
 
 from mathtools.number_theory import divides
 
 class Day(IntEnum):
-    SUNDAY = 0
-    MONDAY = 1
-    TUESDAY = 2
-    WEDNESDAY = 3
-    THURSDAY = 4
-    FRIDAY = 5
-    SATURDAY = 6
+    """
+    The integer values of this enum match with the values used by the `calendar` module:
+    https://docs.python.org/3/library/calendar.html.
+    """
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
 
 months = {
     'January': 31,
@@ -44,20 +49,24 @@ months = {
 def is_leap_year(year: int) -> bool:
     return divides(year, 4) and (divides(year, 400) or not divides(year, 100))
 
+def days_in_year(year: int) -> int:
+    return 366 if is_leap_year(year) else 365
+
 def day_mod(day: Day, days_elapsed: int) -> Day:
+    """The day of the week `days_elapsed` after `day`."""
     return Day((day + days_elapsed) % 7)
 
-def count_sundays_on_first_of_month() -> int:
-    day_of_week = day_mod(Day.MONDAY, 365) # Jan 1 1900 was a Monday -- but we are starting in 1901!
-    total_sundays_on_first_of_month = 0
+def first_day_of_each_month() -> Iterator[Day]:
+    day_of_week = day_mod(Day.MONDAY, days_in_year(1900)) # 1 Jan 1900 was a Monday -- but we are starting in 1901!
+    yield day_of_week
     for year in range(1901, 2001):
         for month, days_in_month in months.items():
             if is_leap_year(year) and month == 'February':
-                day_of_week = day_mod(day_of_week, 29)
-            else:
-                day_of_week = day_mod(day_of_week, days_in_month)
-            if day_of_week == Day.SUNDAY:
-                total_sundays_on_first_of_month += 1
-    return total_sundays_on_first_of_month
+                days_in_month = 29
+            day_of_week = day_mod(day_of_week, days_in_month)
+            yield day_of_week
+
+def count_sundays_on_first_of_month() -> int:
+    return len([day for day in first_day_of_each_month() if day == Day.SUNDAY])
 
 solution = count_sundays_on_first_of_month
